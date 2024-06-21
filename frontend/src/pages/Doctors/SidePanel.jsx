1e2901fc3f404b87ebaeb43d3302142cb8c0d611
Quan Haru <nguyenkimquan2002@gmail.com>
2024-06-21 09:39:42 +0700

    add payment stripe

--- frontend/src/pages/Doctors/SidePanel.jsx
+++ frontend/src/pages/Doctors/SidePanel.jsx
@@ -2,6 +2,32 @@ import React from "react";
 import convertTime from "../../utils/convertTime";
 
 const SidePanel = ({ doctorId, ticketPrice, timeSlots }) => {
+  const bookingHandler = async () => {
+    try {
+      const res = await fetch(
+        `${BASE_URL}/booking/checkout-session/${doctorId}`,
+        {
+          method: "post",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      const data = await res.json();
+
+      if (!res.ok) {
+        throw new Error(data.message + "Please try again");
+      }
+
+      if (data.session.url) {
+        window.location.href = data.session.url;
+      }
+    } catch (err) {
+      toast.error(err.message);
+    }
+  };
+
   return (
     <div className="shadow-panelShadow p-3 lg:p-5 rounded-md">
       <div className="flex items-center justify-between">
@@ -21,14 +47,15 @@ const SidePanel = ({ doctorId, ticketPrice, timeSlots }) => {
                 {item.day.charAt(0).toUpperCase() + item.day.slice(1)}
               </p>
               <p className="text-[15px] leading-6 text-textColor font-semibold">
-                {convertTime(item.startingTime)} -
-                {convertTime(item.endingTime)}
+                {convertTime(item.startingTime)} -{convertTime(item.endingTime)}
               </p>
             </li>
           ))}
         </ul>
       </div>
-      <button className="btn px-2 w-full rounded-md">Book Appointment</button>
+      <button onClick={bookingHandler} className="btn px-2 w-full rounded-md">
+        Book Appointment
+      </button>
     </div>
   );
 };
